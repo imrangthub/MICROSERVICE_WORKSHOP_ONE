faeb757b1fe29ee2a8c50c740a5a70096caff361
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2020-05-21 10:55:47 +0600

    enable Euraka client fro all service with spring-cloud-starter-netflix-eureka-client

--- movie-info-service/src/main/java/com/madbarsoft/movieinfo/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/madbarsoft/movieinfo/MovieInfoServiceApplication.java
@@ -3,7 +3,9 @@ package com.madbarsoft.movieinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MovieInfoServiceApplication {
 
