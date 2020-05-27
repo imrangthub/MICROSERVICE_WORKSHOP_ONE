c74bb2357affb7fd9e0802405a6bc722b155a768
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2020-05-27 17:50:38 +0600

    add Fall back Method with custome config prameter

--- movie-catalog-service/src/main/java/com/madbarsoft/moviecatalog/catalog/UserRatingInfoService.java
+++ movie-catalog-service/src/main/java/com/madbarsoft/moviecatalog/catalog/UserRatingInfoService.java
@@ -1,20 +1,17 @@
 package com.madbarsoft.moviecatalog.catalog;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class UserRatingInfoService {
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
-	
-	
+
 //	@HystrixCommand(fallbackMethod="getFallBackUserRating")
 //	public UserRatingDto getUserRating(String userId) {
 //		UserRatingDto userRatingDto = null;
@@ -35,7 +32,18 @@ public class UserRatingInfoService {
 //		 return userRating;
 //	}
 //	
-	
-	
+
+	@HystrixCommand(fallbackMethod = "getFallBackUserRating", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMillisecinds", value = "2000"),
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+			@HystrixProperty(name = "circuitBreaker.errorThreholdPercentage", value = "50"),
+			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000") })
+	public UserRatingDto getUserRating(String userId) {
+		UserRatingDto userRatingDto = null;
+		userRatingDto = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users2/" + userId,
+				UserRatingDto.class);
+		System.out.println("userRating Update with command Propertis res: " + userRatingDto);
+		return userRatingDto;
+	}
 
 }
