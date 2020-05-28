c9731b4d8d16f1bd3cbd45657efaf566e0481c43
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2020-05-28 17:45:32 +0600

    Init Tow Project (Spring Cloud Config client and server ) for Microservice Level 3

--- movie-info-service/src/main/java/com/madbarsoft/movieinfo/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/madbarsoft/movieinfo/MovieInfoServiceApplication.java
@@ -1,6 +1,5 @@
 package com.madbarsoft.movieinfo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,7 +11,5 @@ public class MovieInfoServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
 	}
-	
-	
 
 }
---
+++ my-spring-cloud-config-client-service1/src/main/java/com/madbarsoft/CloudconfigclientApplication.java
@@ -0,0 +1,15 @@
+package com.madbarsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudconfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudconfigclientApplication.class, args);
+	}
+	
+	
+
+}
